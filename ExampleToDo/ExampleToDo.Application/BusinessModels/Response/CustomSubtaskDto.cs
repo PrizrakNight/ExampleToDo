e2fb557d64fec20a1d694618210df380631b99fb
PrizrakNight <40437643+PrizrakNight@users.noreply.github.com>
2020-09-27 22:24:48 +0300

    Added Response models

---
+++ ExampleToDo/ExampleToDo.Application/BusinessModels/Response/CustomSubtaskDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExampleToDo.Application.BusinessModels.Response
+{
+    public class CustomSubtaskDto : NamedIdentifiableModel
+    {
+        public bool Accomplished { get; set; }
+
+        public DateTime? DateOfCompletion { get; set; }
+    }
+}
---
+++ ExampleToDo/ExampleToDo.Application/BusinessModels/Response/CustomTaskDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExampleToDo.Application.BusinessModels.Response
+{
+    public class CustomTaskDto : NamedIdentifiableModel
+    {
+        public string Description { get; set; }
+
+        public bool Accomplished { get; set; }
+
+        public DateTime? DateOfCompletion { get; set; }
+        public DateTime? Reiteration { get; set; }
+
+        public CustomSubtaskDto[] Subtasks { get; set; }
+    }
+}
