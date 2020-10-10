cb044ef2e57b8450fabad4ee7f43b00375a02d16
PrizrakNight <40437643+PrizrakNight@users.noreply.github.com>
2020-10-10 16:23:42 +0300

    Additional changes

--- ExampleToDo/ExampleToDo.Application/BusinessModels/Response/InvitationDto.cs
+++ ExampleToDo/ExampleToDo.Application/BusinessModels/Response/InvitationDto.cs
@@ -1,7 +1,13 @@
-﻿namespace ExampleToDo.Application.BusinessModels.Response
+﻿using System;
+
+namespace ExampleToDo.Application.BusinessModels.Response
 {
     public class InvitationDto
     {
+        public string Message { get; set; }
 
+        public DateTime SentIn { get; set; }
+
+        public UserBriefInformationDto SenderInformation { get; set; }
     }
 }
--- ExampleToDo/ExampleToDo.Application/BusinessModels/Response/UserBriefInformationDto.cs
+++ ExampleToDo/ExampleToDo.Application/BusinessModels/Response/UserBriefInformationDto.cs
@@ -3,5 +3,6 @@
     public class UserBriefInformationDto
     {
         public string UserName { get; set; }
+        public string Email { get; set; }
     }
 }
